40f60ace1213b5c021187177bc22d156ef52c8b5
seandooa <seandooa@yahoo.com.sg>
2024-02-03 18:26:33 +0800

    Clean up Contacts0

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 
 public class Contacts0 {
 
+    public static final int MAX_ENTRIES = 100;
+
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
-        System.out.println("|| ===================================================");
-        System.out.println("|| ===================================================");
-        System.out.println("|| Contacts - Version 0.0");
-        System.out.println("|| Welcome to Contacts!");
-        System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+        printWelcomeMessage();
+        String[][] list = new String[MAX_ENTRIES][3];
         int count = 0;
         while (true) {
             System.out.print("|| " + "Enter command: ");
+
             String inputLine = SCANNER.nextLine();
             while (inputLine.trim().isEmpty() || inputLine.trim().charAt(0) == '#') {
                 inputLine = SCANNER.nextLine();
@@ -144,6 +143,14 @@ public class Contacts0 {
                 System.out.println("|| " + m);
             }
         }
+    }
+
+    private static void printWelcomeMessage() {
+        System.out.println("|| ===================================================");
+        System.out.println("|| ===================================================");
+        System.out.println("|| Contacts - Version 1.0");
+        System.out.println("|| Welcome to Contacts!");
+        System.out.println("|| ===================================================");
     }
 
 }
